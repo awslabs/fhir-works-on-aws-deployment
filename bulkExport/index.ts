88105a1b6bec77159016e25bf13dee866143de07
Nestor Carvantes <carvantes@gmail.com>
2020-10-09 12:19:30 -0700

    feat: Add Crawler and startCrawler Lambda (#120)

---
+++ bulkExport/index.ts
@@ -0,0 +1,8 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
+import { startCrawlerHandler } from 'fhir-works-on-aws-persistence-ddb';
+
+exports.startCrawlerHandler = startCrawlerHandler;
