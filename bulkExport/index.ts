ad0dbcab0a0790dd8e8a70123b9c4057a5ef08f3
Nestor Carvantes <nestorba@amazon.com>
2020-10-15 12:13:13 -0700

    feat: add lambda fns to start glue jobs and check its status

--- bulkExport/index.ts
+++ bulkExport/index.ts
@@ -3,6 +3,8 @@
  *  SPDX-License-Identifier: Apache-2.0
  */
 
-import { startCrawlerHandler } from 'fhir-works-on-aws-persistence-ddb';
+import { startCrawlerHandler, startExportJobHandler, getJobStatusHandler } from 'fhir-works-on-aws-persistence-ddb';
 
 exports.startCrawlerHandler = startCrawlerHandler;
+exports.startExportJobHandler = startExportJobHandler;
+exports.getJobStatusHandler = getJobStatusHandler;
