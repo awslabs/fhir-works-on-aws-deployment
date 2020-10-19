ea6b222444153e1d8b2b10c5bb7390f60bd7d64f
Nestor Carvantes <nestorba@amazon.com>
2020-10-19 15:37:17 -0700

    feat: add bulk export updateStatus lambda

--- bulkExport/index.ts
+++ bulkExport/index.ts
@@ -3,8 +3,14 @@
  *  SPDX-License-Identifier: Apache-2.0
  */
 
-import { startCrawlerHandler, startExportJobHandler, getJobStatusHandler } from 'fhir-works-on-aws-persistence-ddb';
+import {
+    startCrawlerHandler,
+    startExportJobHandler,
+    getJobStatusHandler,
+    updateStatusStatusHandler,
+} from 'fhir-works-on-aws-persistence-ddb';
 
 exports.startCrawlerHandler = startCrawlerHandler;
 exports.startExportJobHandler = startExportJobHandler;
 exports.getJobStatusHandler = getJobStatusHandler;
+exports.updateStatusStatusHandler = updateStatusStatusHandler;
