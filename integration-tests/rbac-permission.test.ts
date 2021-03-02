aca6c881dc09b4060c537506ef8544673623c262
Tim Nguyen <thingut@amazon.com>
2021-03-02 15:52:09 -0500

    test: Add RBAC Permission test

---
+++ integration-tests/rbac-permission.test.ts
@@ -0,0 +1,36 @@
+import { getFhirClient } from './utils';
+
+const patientRecord = {
+    resourceType: 'Patient',
+    name: [
+        {
+            family: 'Smith',
+            given: ['John'],
+        },
+    ],
+    gender: 'male',
+};
+
+test('practitioner role can create new patient', async () => {
+    const client = await getFhirClient(false);
+    await expect(client.post('Patient', patientRecord)).resolves.toMatchObject({
+        status: 201,
+        data: patientRecord,
+    });
+});
+
+describe('Negative tests', () => {
+    test('invalid token', async () => {
+        const client = await getFhirClient(false, 'Invalid token');
+        await expect(client.post('Patient', patientRecord)).rejects.toMatchObject({
+            response: { status: 401 },
+        });
+    });
+
+    test('auditor role cannot create new patient record', async () => {
+        const client = await getFhirClient(true);
+        await expect(client.post('Patient', patientRecord)).rejects.toMatchObject({
+            response: { status: 403 },
+        });
+    });
+});
