f458d83f6acf356d31d4f2b699253a13354878fd
Tim Nguyen <thingut@amazon.com>
2021-03-03 14:31:04 -0500

    Remove accidental code change

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,7 @@ import serverless from 'serverless-http';
 import { generateServerlessRouter } from 'fhir-works-on-aws-routing';
 import { fhirConfig, genericResources } from './config';
 
-const defaultCorsOptions = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-};
-
-const serverlessHandler = serverless(generateServerlessRouter(fhirConfig, genericResources, {}), {
+const serverlessHandler = serverless(generateServerlessRouter(fhirConfig, genericResources), {
     request(request: any, event: any) {
         request.user = event.user;
     },
