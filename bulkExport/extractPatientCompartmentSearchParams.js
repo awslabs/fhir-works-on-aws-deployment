a42afbdc4386b06de710a552291638a1e8e9968c
zheyanyu <zheyanyu@amazon.com>
2021-07-22 15:49:51 -0400

    update script to accomadate v3 and v4 definition

--- bulkExport/extractPatientCompartmentSearchParams.js
+++ bulkExport/extractPatientCompartmentSearchParams.js
@@ -1,26 +1,49 @@
-const compartmentPatient = require('./compartmentdefinition-patient.json');
-const baseSearchParams = require('../../fhir-works-on-aws-search-es/src/schema/compiledSearchParameters.4.0.1.json');
+const fs = require('fs');
+const compartmentPatientV3 = require('./schema/compartmentdefinition-patient.3.0.2.json');
+const compartmentPatientV4 = require('./schema/compartmentdefinition-patient.4.0.1.json');
+const baseSearchParamsV3 = require('../../fhir-works-on-aws-search-es/src/schema/compiledSearchParameters.3.0.1.json');
+const baseSearchParamsV4 = require('../../fhir-works-on-aws-search-es/src/schema/compiledSearchParameters.4.0.1.json');
 
 // Create a dictionary of search params
-/* example of an item in baseSearchParamsDict:
-   Account-identifier: {resourceType: "Account", path: "identifier"}
- */
-const baseSearchParamsDict = {};
-baseSearchParams.forEach(param => {
-    baseSearchParamsDict[`${param.base}-${param.name}`] = param.compiled;
-});
+function extractPatientCompartmentSearchParams(baseSearchParams, compartmentPatient) {
+    const baseSearchParamsDict = {};
+    // example of an item in baseSearchParamsDict: Account-identifier: {resourceType: "Account", path: "identifier"}
+    baseSearchParams.forEach(param => {
+        baseSearchParamsDict[`${param.base}-${param.name}`] = param.compiled;
+    });
 
-// Find the search params needed for patient compartment
-const patientCompartmentSearchParams = {};
-compartmentPatient.resource.forEach(resource => {
-    if (resource.param) {
-        let compiledPaths = [];
-        resource.param.forEach(param => {
-            const pathsForThisParam = baseSearchParamsDict[`${resource.code}-${param}`].map(item => item.path);
-            compiledPaths = compiledPaths.concat(pathsForThisParam);
-        });
-        patientCompartmentSearchParams[resource.code] = compiledPaths;
-    }
-});
+    // Find the search params needed for patient compartment
+    const patientCompartmentSearchParams = {};
+    compartmentPatient.resource.forEach(resource => {
+        if (resource.param) {
+            let compiledPaths = [];
+            resource.param.forEach(param => {
+                const pathsForThisParam = baseSearchParamsDict[`${resource.code}-${param}`].map(item => item.path);
+                compiledPaths = compiledPaths.concat(pathsForThisParam);
+            });
+            patientCompartmentSearchParams[resource.code] = compiledPaths;
+        }
+    });
+    return patientCompartmentSearchParams;
+}
 
-console.log(patientCompartmentSearchParams);
+const patientCompartmentSearchParamsV4 = extractPatientCompartmentSearchParams(
+    baseSearchParamsV4,
+    compartmentPatientV4,
+);
+const patientCompartmentSearchParamsV3 = extractPatientCompartmentSearchParams(
+    baseSearchParamsV3,
+    compartmentPatientV3,
+);
+
+fs.writeFileSync(
+    './schema/patientCompartmentSearchParams.3.0.2.json',
+    JSON.stringify(patientCompartmentSearchParamsV3),
+);
+fs.writeFileSync(
+    './schema/patientCompartmentSearchParams.4.0.1.json',
+    JSON.stringify(patientCompartmentSearchParamsV4),
+);
+
+console.log(patientCompartmentSearchParamsV4);
+console.log(patientCompartmentSearchParamsV3);
