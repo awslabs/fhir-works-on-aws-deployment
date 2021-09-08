52d1bb36b41774441f338755875dcc447202a7f3
Tim Nguyen <thingut@amazon.com>
2021-09-08 11:56:34 -0400

    chore: Round 2. Delete extraneous new lines

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
 /*
-
  *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
  *  SPDX-License-Identifier: Apache-2.0
-
  */
-
 
 import serverless from 'serverless-http';
 
@@ -13,20 +9,12 @@ import { generateServerlessRouter } from 'fhir-works-on-aws-routing';
 
 import { fhirConfig, genericResources } from './config';
 
-
 const serverlessHandler = serverless(generateServerlessRouter(fhirConfig, genericResources, {}), {
-
     request(request: any, event: any) {
-
         request.user = event.user;
-
     },
-
 });
 
-
 export default async (event: any = {}, context: any = {}): Promise<any> => {
-
     return serverlessHandler(event, context);
-
 };
