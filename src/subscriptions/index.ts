46e6bdbb1630a7cb9f748c49ccb7d745524000f6
ssvegaraju <ssvegaraju@yahoo.co.in>
2022-02-08 02:23:51 +0000

    finish implementation

--- src/subscriptions/index.ts
+++ src/subscriptions/index.ts
@@ -5,7 +5,13 @@
  */
 
 import axios from 'axios';
+import AWS from 'aws-sdk';
 import { DynamoDbDataService } from 'fhir-works-on-aws-persistence-ddb/src/dataServices/dynamoDbDataService';
+
+const enableMultiTenancy = process.env.ENABLE_MULTI_TENANCY === 'true';
+const dbService = new DynamoDbDataService(new AWS.DynamoDB(), false, {
+    enableMultiTenancy: enableMultiTenancy,
+});
 
 const sendCfnResponse = async (event: any, status: 'SUCCESS' | 'FAILED', error?: Error) => {
     if (error !== undefined) {
@@ -29,9 +35,26 @@ const sendCfnResponse = async (event: any, status: 'SUCCESS' | 'FAILED', error?:
  * Custom lambda handler that handles deleting expired subscriptions.
  */
 exports.handler = async (event: any) => {
-    console.log(event);
     try {
-        
+        const subscriptions = await dbService.getActiveSubscriptions({});
+        const currentTime = new Date();
+        let subTime;
+        const deletions: Promise<{
+            success: boolean,
+            message: string,
+        }>[] = [];
+        // filter out subscriptions without a defined end time.
+        subscriptions.filter(s => s.end).forEach(subscription => {
+            // check if subscription is past its end date (ISO format)
+            // example format of subscriptions: https://www.hl7.org/fhir/subscription-example.json.html
+            subTime = new Date(subscription.end);
+            if (currentTime >= subTime) {
+                // delete the subscription as it has reached its end time
+                deletions.push(dbService.deleteResource({ resourceType: subscription.resourceType, id: subscription.id }));
+            }
+        });
+        await Promise.all(deletions);
+        await sendCfnResponse(event, 'SUCCESS');
     } catch (e) {
         await sendCfnResponse(event, 'FAILED', e);
     }
