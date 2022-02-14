56cf3f8bebde7d77264f1aede8731999ceec7412
Sukeerth Vegaraju <ssvegaraju@yahoo.co.in>
2022-02-14 09:56:51 -0800

    feat: subscriptionReaper (#557)

--- src/subscriptions/index.ts
+++ src/subscriptions/index.ts
@@ -1,3 +1,10 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+import reaperHandler from './subscriptionReaper';
 import RestHookHandler from './restHook';
 import { AllowListInfo, getAllowListInfo } from './allowListUtil';
 
@@ -12,3 +19,8 @@ const restHookHandler = new RestHookHandler({ enableMultitenancy });
 exports.handler = async (event: any) => {
     return restHookHandler.sendRestHookNotification(event, allowListPromise);
 };
+
+/**
+ * Custom lambda handler that handles deleting expired subscriptions.
+ */
+exports.reaperHandler = reaperHandler;
