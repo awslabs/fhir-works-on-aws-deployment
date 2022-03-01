e7c0b3808467cfaf614512456aead92fc0ecd01b
Nestor Carvantes <carvantes@gmail.com>
2022-03-01 01:17:22 -0800

    docs: add Subscription docs

--- src/subscriptions/allowList.ts
+++ src/subscriptions/allowList.ts
@@ -5,14 +5,39 @@
 
 import { SubscriptionEndpoint } from 'fhir-works-on-aws-routing/lib/router/validation/subscriptionValidator';
 
+/**
+ * Return the list of endpoints that are allowed in Subscriptions.
+ * When multi-tenancy is enabled you must specify the tenantId associated with each endpoint.
+ */
 const getAllowListedSubscriptionEndpoints = async (): Promise<SubscriptionEndpoint[]> => {
     return [];
-    // Add here the endpoints that are allowed in Subscriptions
     // [
+    // Schema:
     //     {
     //         endpoint: string | RegExp;
     //         headers?: string[];
     //         tenantId? string;
+    //     }
+    //
+    // Simple example:
+    //     {
+    //         endpoint: 'https://my-endpoint.com/some-path',
+    //         headers: ['some-header: some-value']
+    //         tenantId: 'tenant-1',
+    //     }
+    //
+    // RegExp example:
+    //     {
+    //         endpoint: /^https:\/\/my-endpoint.com\/.*/,
+    //     }
+    //
+    // When using Authorization headers, it is recommended to store them securely in an external system such as AWS Secrets Manager.
+    // Example:
+    //     {
+    //         endpoint: 'https://my-endpoint.com',
+    //         headers: [
+    //              `Authorization: ${(await secretsManagerClient.getSecretValue({ SecretId: 'YOUR_SECRET_ID' })).data.SecretString}`;
+    //         ],
     //     }
     //     ...
     // ]
