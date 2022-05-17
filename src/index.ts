b20c9a349014ba9f130808248abadaeb9035c98e
Mike Lohmeier <mike.lohmeier@resmed.com>
2022-05-17 17:18:33 -0500

    feat: ISSUE 624 add logging when lambda starts and stops fwoa application code in the fhirServer lambda

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,21 @@
  */
 
 import serverless from 'serverless-http';
+import { partial } from 'lodash';
 import { generateServerlessRouter } from 'fhir-works-on-aws-routing';
+import { makeLogger } from 'fhir-works-on-aws-interface';
 import { getFhirConfig, genericResources } from './config';
+
+// setup logging for process start and stops
+const logger = makeLogger({ pid: process.pid, ppid: process.ppid });
+const logProcessEvent = (eventName: string, codeOrSignal: any) => {
+    logger.info('process event raised', { eventName, codeOrSignal });
+};
+logProcessEvent('start', undefined);
+process.on('beforeExit', partial(logProcessEvent, 'beforeExit'));
+process.on('exit', partial(logProcessEvent, 'exit'));
+process.on('SIGTERM', partial(logProcessEvent, 'SIGTERM'));
+process.on('SIGINT', partial(logProcessEvent, 'SIGINT'));
 
 const ensureAsyncInit = async (initPromise: Promise<any>): Promise<void> => {
     try {
