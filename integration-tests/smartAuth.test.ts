f3460338bb34a640c0ea8a86104f4f690cce7d3f
Robert Smayda <smayda44@gmail.com>
2022-11-21 14:03:49 -0500

    test: add more negative integration tests

--- integration-tests/smartAuth.test.ts
+++ integration-tests/smartAuth.test.ts
@@ -1,11 +1,21 @@
 import { AxiosInstance } from 'axios';
-import { getFhirClient, idsOfFhirResources } from './utils';
+import { getFhirClient, idsOfFhirResources, randomPatient } from './utils';
 
 const sherlockId = idsOfFhirResources.sherlockHolmes;
 const mycroftId = idsOfFhirResources.mycroftHolmes;
 
 async function getPatient(client: AxiosInstance, patientId: string) {
     const url = `/Patient/${patientId}`;
+    return client.get(url);
+}
+
+async function postPatient(client: AxiosInstance) {
+    const url = '/Patient/';
+    return client.post(url, randomPatient());
+}
+
+async function getEncounter(client: AxiosInstance, encounterId: string) {
+    const url = `/Encounter/${encounterId}`;
     return client.get(url);
 }
 
@@ -20,7 +30,7 @@ test('Successfully read own user record and records that reference the user', as
     expect(mycroftRecord.data.name[0].given).toEqual(['Mycroft']);
 });
 
-describe('Negative tests', () => {
+describe('SMART AuthZ Negative tests', () => {
     test('Access token with insufficient scope', async () => {
         // FhirClient does not include enough permission because patient/Patient.read scope is missing
         const fhirClient = await getFhirClient('launch/patient', false);
@@ -46,6 +56,55 @@ describe('Negative tests', () => {
 
         await expect(getPatient(fhirClient, watsonId)).rejects.toMatchObject({
             response: { status: 401 },
+        });
+    });
+
+    const nonAdminScopes: string[] = [
+        'patient/Patient.read',
+        'user/Patient.read',
+        'patient/Patient.read user/Patient.read',
+    ];
+    const adminScopes: string[] = [
+        'patient/Patient.read',
+        'user/Patient.read',
+        'system/Patient.read',
+        'patient/Patient.read user/Patient.read',
+        'patient/Patient.read system/Patient.read',
+        'system/Patient.read user/Patient.read',
+        'patient/Patient.read system/Patient.read user/Patient.read',
+    ];
+    describe.each(nonAdminScopes)('NON-ADMIN with scope: (%p)', (scope: string) => {
+        let fhirClient: AxiosInstance;
+
+        beforeAll(async () => {
+            fhirClient = await getFhirClient(`launch/patient fhirUser ${scope}`, false);
+        });
+        test('Attempt to READ an encounter failing due to scoped resource', async () => {
+            await expect(getEncounter(fhirClient, '1234')).rejects.toMatchObject({
+                response: { status: 401 },
+            });
+        });
+        test('Attempt to WRITE a Patient failing due to incorrect access scope', async () => {
+            await expect(postPatient(fhirClient)).rejects.toMatchObject({
+                response: { status: 401 },
+            });
+        });
+    });
+    describe.each(adminScopes)('ADMIN with scope: (%p)', (scope: string) => {
+        let fhirClient: AxiosInstance;
+
+        beforeAll(async () => {
+            fhirClient = await getFhirClient(`launch/patient fhirUser ${scope}`, true);
+        });
+        test('Attempt to READ an encounter failing due to scoped resource', async () => {
+            await expect(getEncounter(fhirClient, '1234')).rejects.toMatchObject({
+                response: { status: 401 },
+            });
+        });
+        test('Attempt to WRITE a Patient failing due to incorrect access scope', async () => {
+            await expect(postPatient(fhirClient)).rejects.toMatchObject({
+                response: { status: 401 },
+            });
         });
     });
 });
