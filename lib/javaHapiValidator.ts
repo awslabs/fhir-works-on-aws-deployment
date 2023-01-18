254016dd11a9e0a756bb02bee6003f77bac96565
ssvegaraju <ssvegaraju@yahoo.co.in>
2023-01-18 00:54:54 -0800

    add back s3 bucket deployment method

--- lib/javaHapiValidator.ts
+++ lib/javaHapiValidator.ts
@@ -2,7 +2,9 @@ import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { Alias, Code, Function, Runtime, Tracing } from 'aws-cdk-lib/aws-lambda';
 import { RetentionDays } from 'aws-cdk-lib/aws-logs';
 import { Construct } from 'constructs';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import * as path from 'path';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 
 export interface JavaHapiValidatorProps extends StackProps {
     stage: string;
@@ -18,9 +20,15 @@ export default class JavaHapiValidator extends Stack {
     constructor(scope: Construct, id: string, props: JavaHapiValidatorProps) {
         super(scope, id, props);
 
+        const igBucket = new Bucket(scope, 'ImplementationGuidesBucket');
+        const igDeployment = new BucketDeployment(scope, 'IGFiles', {
+            sources: [Source.asset(path.resolve(__dirname, '../implementationGuides'))],
+            destinationBucket: igBucket,
+        });
+
         this.hapiValidatorLambda = new Function(scope, 'validator', {
             handler: 'software.amazon.fwoa.Handler',
-            timeout: Duration.seconds(25),
+            timeout: Duration.seconds(300),
             memorySize: 2048,
             currentVersionOptions: {
                 provisionedConcurrentExecutions: 5,
@@ -34,6 +42,7 @@ export default class JavaHapiValidator extends Stack {
             tracing: Tracing.ACTIVE,
             environment: {
                 FHIR_VERSION: props.fhirVersion,
+                IMPLEMENTATION_GUIDES_BUCKET: igDeployment.deployedBucket.bucketName,
             },
         });
         this.alias = this.hapiValidatorLambda.currentVersion.addAlias(`fhir-service-validator-lambda-${props.stage}`);
